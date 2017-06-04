2789733b588a89d4408db4686437317b36154520
SilentObserver <bsuniljose25@gmail.com>
2017-06-04 11:39:23 +0530

    Modularized the following scripts: mangafox_full_ripper.py mangafox_index_ripper.py

--- mangafox_full_ripper.py
+++ mangafox_full_ripper.py
@@ -1,20 +1,49 @@
-from mangafox_index_ripper import scrape_manga_index
-from ProgressBar import update_progress
-import mangafox_ripper as mr
+import os
 import time
 
+import mangafox_index_ripper as index_ripper
+import mangafox_ripper as chapter_ripper
+from ProgressBar import update_progress
+
+
+def download_complete_manga(manga_index_link):
+    chapter_links = index_ripper.get_manga_indexes(manga_index_link)
+    completed_links_file = open("completed_links.inli", "r")
+    completed_links = completed_links_file.read().split("\n")
+    completed_links_file.close()
+    completed_links_file = open("completed_links.inli", "a")
+    sleep_counter = 0
+    completed_count = 0
+    update_progress(0)
+    try:
+        for x in chapter_links:
+            if x not in completed_links:
+                chapter_ripper.download_chapter(x)
+                sleep_counter += 1
+                completed_links.append(x)
+                completed_links_file.write(x)
+                completed_links_file.write("\n")
+                completed_links_file.flush()
+            completed_count += 1
+
+            if sleep_counter == 5:
+                time.sleep(5)
+                sleep_counter = 0
+            update_progress(completed_count / len(chapter_links))
+        return True
+    except:
+        print("Error occurred while downloading")
+
+
 if __name__ == '__main__':
-	manga_index_link = input("Enter the link to the manga index:")
-	chapter_links = scrape_manga_index(manga_index_link)
-	sleep_counter = 0
-	completed_count = 0
-	update_progress(0)
-	for x in chapter_links:
-		mr.download_chapter(x)
-		sleep_counter +=1
-		completed_count += 1
-		if sleep_counter == 5:
-			time.sleep(5)
-			sleep_counter = 0
-		update_progress(completed_count/len(chapter_links))
-	print("\nDownload Complete")
+    print("Version1.2")
+    index_link = input("Enter the link to the manga index:")
+    status = download_complete_manga(index_link)
+    if status:
+        print("\nDownload Complete")
+        # Desktop Notification in Ubuntu
+        os.system("notify-send \"Download Complete\" \"Completed Downloading manga\"")
+    else:
+        print("\nDownload Failed")
+        # Desktop Notification in Ubuntu
+        os.system("notify-send \"Download Failed\" \"Failed to Download manga\"")
--- mangafox_index_ripper.py
+++ mangafox_index_ripper.py
@@ -1,18 +1,26 @@
-import urllib as ul
-from bs4 import BeautifulSoup as bs
 import requests as rq
+from bs4 import BeautifulSoup as bs
 
-def scrape_manga_index(manga_index_page):
-	manga_index_soup = bs(rq.get(manga_index_page).content,'lxml')
-	list_of_all_chapter_link_tags = manga_index_soup.find_all("a",{"class":"tips"})
+
+def get_manga_indexes(manga_index_page):
+	manga_index_soup = bs(rq.get(manga_index_page).content, 'lxml')
+	list_of_all_chapter_link_tags = manga_index_soup.find_all("a", {"class": "tips"})
 	list_of_all_chapter_links = []
 	for link in list_of_all_chapter_link_tags:
 		list_of_all_chapter_links.append(link.get('href'))
 	list_of_all_chapter_links.sort()
 	return list_of_all_chapter_links
+
+
+def write_manga_indexes_to_file(manga_index_page, index_file_name):
+	outFile = open(index_file_name, "w")
+	for index_link in get_manga_indexes(manga_index_page):
+		outFile.write(index_link)
+		outFile.write("\n")
+	outFile.close()
+
+
 if __name__ == '__main__':
 	manga_index_page_link = input("Manga index page:")
-	chapter_links = scrape_manga_index(manga_index_page_link)
-	for x in chapter_links:
-		print(x)
-	print("No. of Chapters:",len(chapter_links))
+	output_file_name = input("Output file name:")
+	write_manga_indexes_to_file(manga_index_page_link, output_file_name)
